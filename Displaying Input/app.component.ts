eb890337eec0948fe43df622e53421f4deb44e48
prathameshtekade <105971071+prathameshtekade@users.noreply.github.com>
2022-08-16 11:55:50 +0530

    Add files via upload

---
+++ Displaying Input/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FIRSTANGULARPROJECT';
+  getData(val:string){
+    console.warn(val);
+  }
+}
